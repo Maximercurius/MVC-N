c6e1780d37955807b925f5fd8e3961bfd4ee37a7
Maxim <91497610+Maximercurius@users.noreply.github.com>
2022-02-14 23:05:17 +0500

    Update AppDelegate.swift

--- MVC-N/Application/Controller/AppDelegate.swift
+++ MVC-N/Application/Controller/AppDelegate.swift
@@ -12,6 +12,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    //commit test file
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
